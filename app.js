3292024126520fa59d270e5d9c3aa283a044eb5c
AnasAhmad96 <oudatanas96@gmail.com>
2021-06-22 13:54:44 +0300

    lab01

---
+++ app.js
@@ -0,0 +1,9 @@
+let ans=prompt('are you ready to be web developer ?');
+console.log(ans);
+let ans1=prompt('how old are you ?');
+console.log(ans1);
+let ans2=prompt('Enter yor name?');
+console.log(ans2);
+let ans3=prompt('what is your major?');
+console.log(ans3);
+alert('Welcome Dear')
